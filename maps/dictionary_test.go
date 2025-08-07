4f3930907cb5cf320b4a225bc7561bbd3193a05f
Qendrim Behrami <behrami@in.tum.de>
2025-08-07 19:36:42 +0200

    Add first half of maps chapter

---
+++ maps/dictionary_test.go
@@ -0,0 +1,43 @@
+package maps
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("Expected to get an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
+
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, err error) {
+	t.Helper()
+
+	if got != err {
+		t.Errorf("got error %q want %q", got, err)
+	}
+}
